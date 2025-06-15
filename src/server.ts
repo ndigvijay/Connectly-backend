71629c9462ecbb3725b01d73ec1449cb336b816b
Digvijay Narayan <digvijayn1993@gmail.com>
2025-06-15 15:19:20 +0530

    add frontend url

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? process.env.PROD_REST_API_URL || 'https://your-frontend-domain.com'
+        ? process.env.FRONTEND_URL || 'https://your-frontend-domain.com'
         : [
             'http://localhost:3000',
             'http://localhost:3001',
